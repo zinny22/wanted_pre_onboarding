db921a0d399487104f46ad7fee3baf3e3bf9cd6e
zinny22 <hyejinsang@naver.com>
2022-04-18 17:06:40 +0900

    기본 골격

---
+++ wanted_pre_onboarding/src/components/Dropdown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Dropdown =()=>{
+  return(
+    <React.Fragment>
+
+    </React.Fragment>
+  )
+}
+
+export default Dropdown;
---
+++ wanted_pre_onboarding/src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Slider =()=>{
+  return(
+    <React.Fragment>
+
+    </React.Fragment>
+  )
+}
+
+export default Slider;
